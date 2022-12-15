3377b4076c27cf6fb0e07f0d9fb15c8c8af83d91
dmacsandwich <120608633+dmacsandwich@users.noreply.github.com>
2022-12-15 15:58:55 -0600

    Initial commit
    Empty project commit

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,16 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
-{   [SerializeField] float rotationThrust = 1f;
+{   
+    [SerializeField] float rotationThrust = 1f;
     [SerializeField] float mainThrust = 15;
-    Rigidbody rb; 
+    Rigidbody rb;
+    AudioSource rocketThrustAudio;
+
 
     // Start is called before the first frame update
     void Start() // initiated at beggining
     {
         rb = GetComponent<Rigidbody>();
+        rocketThrustAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,7 +30,13 @@ public class MovePlayer : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust);
+            ProcessRocketAudio(true);
         }
+        else
+        {
+            ProcessRocketAudio(false);
+        }
+
     }
 
     void  ProcessRotation()
@@ -38,14 +49,28 @@ public class MovePlayer : MonoBehaviour
             {
                 ApplyRotation(-rotationThrust);
             }
+    }
 
     void ApplyRotation(float rotationThisFrame)
     {
         rb.freezeRotation = true; //freezing rotation so we can manually rotate
         transform.Rotate(Vector3.forward * rotationThisFrame);
         rb.freezeRotation = false; // unfreeze rotation so physics system can take over
-    }
-    
+    } 
+
+    void ProcessRocketAudio(bool rocketAudio)
+    {
+        if (rocketAudio)
+        {   
+            if(!rocketThrustAudio.isPlaying)
+            {
+                rocketThrustAudio.Play();
+            }
+        }
+        else
+        {
+            rocketThrustAudio.Stop();
+        }
 
     }
 }
